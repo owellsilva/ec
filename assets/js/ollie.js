c38713f0e59ea0658d3843f1067a2876bd98ee1b
santiagoher <124633203+santiagoher@users.noreply.github.com>
2023-02-11 06:18:51 +0000

    Updates

--- assets/js/ollie.js
+++ assets/js/ollie.js
@@ -46,3 +46,13 @@ $('#owl-testmonial').owlCarousel({
     nav: true,
     dots: false
 })
+
+//Menu
+const menu_hamburguer = document.querySelector('.menu_hamburguer');
+const nav_list = document.querySelector('.header__nav--list');
+
+menu_hamburguer.addEventListener("click", ({currentTarget}) => {
+  const currentTargetChild = currentTarget.firstElementChild;
+  currentTargetChild.classList.toggle('active');
+  nav_list.classList.toggle('open');
+});
